b24f995fcc02e38f5cb8b47336facc496ecdb4fe
Sam Marder <sam.marder@useriq.com>
2015-05-09 18:04:39 -0400

    Added multithreading.
    Defaults to 10 threads (uses about 6KB/s up and down combined).

--- main.py
+++ main.py
@@ -14,34 +14,52 @@ import ipgetter
 from netaddr import IPNetwork
 from sys import argv
 import csv
+from threading import Thread, Lock
+
+logger = logging.getLogger('backbone-tracer')
 
 def get_ip_from_range(ip_range):
     ip = IPNetwork(ip_range).ip
     return ip + 1
 
+def skip_beginning(itr, offset):
+    for i in range(offset):
+        itr.next()
+    return itr
+
+def compute_trace(source, row):
+    destination = get_ip_from_range(row['network'])
+    logging.info("Tracerouting: %s" % destination)
+    output = traceroute(source, str(destination))
+    output.latitude = row['latitude']
+    output.longitude = row['longitude']
+    return output
+
+def write_trace(order, output):
+    if not writer.write(order, output.__dict__):
+        logger.warning("Failed to write %s" % output.__dict__)
+
+def tracer(source, itr):
+    for i, row in itr:
+        trace = compute_trace(source, row)
+        write_trace(i, trace)
+
 def generate_data(traceroute, input_filename, writer):
-    logger = logging.getLogger('backbone-tracer')
     logger.info('Backbone Tracer is tracing IPs..')
     source = ipgetter.myip()
     logger.info('My IP address is: %s' % source)
     with open(input_filename) as csvfile:
         reader = csv.DictReader(csvfile)
         offset = writer.count()
+        itr = enumerate(skip_beginning(reader, offset))
         logger.info("Skipping past the first %d addresses." % offset)
-        for row in reader:
-            if offset > 0:
-                offset -= 1
-                continue
-            destination = get_ip_from_range(row['network'])
-            logger.info("Tracerouting: %s" % destination)
-            output = traceroute(source, str(destination))
-            output.latitude = row['latitude']
-            output.longitude = row['longitude']
-            try:
-                if not writer.write(output.__dict__):
-                    logger.warning("Failed to write %s" % output.__dict__)
-            except ValueError, e:
-                logger.warning(e)
+        threads = []
+        for i in range(10):
+            thread = Thread(target=tracer, args=(source, itr))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
--- writers.py
+++ writers.py
@@ -1,25 +1,34 @@
 import json
 
-class Writer(object):
-    def write(self, trace):
-        raise NotImplementedError("Use a concrete writer!")
-
-    def count(self):
-        raise NotImplemenetedError("Use a concrete writer!")
-
-class JsonWriter(Writer):
+class JsonWriter(object):
     def __init__(self, filename):
         self.filename = filename
+        self.next_to_write = 0
+        self.waiting = {}
 
-    def write(self, trace):
+    def _queue(self, i, trace):
+        self.waiting[i] = trace
+
+    def _write_queue(self):
+        with open(self.filename, "a") as f:
+            while self.waiting.has_key(self.next_to_write):
+                trace = self.waiting[self.next_to_write]
+                s = json.dumps(trace)
+                f.write(s + '\n')
+                del self.waiting[self.next_to_write]
+                self.next_to_write += 1
+
+    def write(self, order, trace):
         if not trace:
             return False
-        with open(self.filename, "a") as f:
-            s = json.dumps(trace)
-            f.write(s + '\n')
+        self._queue(order, trace)
+        self._write_queue()
         return True
 
     def count(self):
-        with open(self.filename) as f:
-            return sum (1 for line in f)
+        try:
+            with open(self.filename) as f:
+                return sum (1 for line in f)
+        except IOError:
+            return 0
 
