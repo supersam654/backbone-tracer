9c855959d704faebacd9c39846bb1f72add59f69
Sam Marder <sam.marder@useriq.com>
2015-05-09 18:47:01 -0400

    Fixed a threading race condition.

--- writers.py
+++ writers.py
@@ -1,23 +1,26 @@
 import json
 import gzip
+from threading import Lock
 
 class JsonWriter(object):
     def __init__(self, filename):
         self.filename = filename
         self.next_to_write = 0
         self.waiting = {}
+        self.lock = Lock()
 
     def _queue(self, i, trace):
         self.waiting[i] = trace
 
     def _write_queue(self):
-        with gzip.open(self.filename, "a") as f:
-            while self.waiting.has_key(self.next_to_write):
-                trace = self.waiting[self.next_to_write]
-                s = json.dumps(trace)
-                f.write(s + '\n')
-                del self.waiting[self.next_to_write]
-                self.next_to_write += 1
+        with self.lock:
+            with gzip.open(self.filename, "a") as f:
+                while self.waiting.has_key(self.next_to_write):
+                    trace = self.waiting[self.next_to_write]
+                    s = json.dumps(trace)
+                    f.write(s + '\n')
+                    del self.waiting[self.next_to_write]
+                    self.next_to_write += 1
 
     def write(self, order, trace):
         if not trace:
